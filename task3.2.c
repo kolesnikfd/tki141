0c1fbf916b90fe0675c27ecd0caa13b4808ac759
Колесников Фёдор ТКИ-141 <kolesnikow_fedor@mail.ru>
2025-10-25 20:16:25 +0300

    Add files via upload

---
+++ task3.2.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * @brief считывает целое значение с клавиатуры в проверкой ввода
+ * @return возвращает считанное значение
+ */
+int getValue();
+
+/**
+ * @brief считывает вещественное значение с клавиатуры в проверкой ввода
+ * @return возвращает считанное значение
+ */
+double getDouble();
+
+/**
+ * @brief рассчитывает сумму n членов последовательности
+ * @param n - заданное число членов
+ * @return рассчитанное значение
+ */
+double getSumN(const int n);
+
+/**
+ * @brief рассчитывает сумму членов последовательности,
+ * с точностью e
+ * @param e - заданная точность
+ * @return рассчитанное значение
+ */
+double getSumE(const double e);
+
+/**
+ * @brief рассчитывает коэффициент рекуррентного выражения
+ * @param i текущий индекс
+ * @return рассчитанное значение коэффициента
+ */
+double getRecurent(const int i);
+
+/** 
+ * @brief проверяет, что число положительное
+ * @param value - проверяемое значение
+ */
+void checkPositive(const double value);
+
+/**
+ * @brief рассчитывает факториал числа
+ * @param n - заданное число
+ * @return рассчитанное значение
+ */
+int factorial(const int n);
+
+/**
+ * @brief ищет количество делителей числа n, которые больше k
+ * @param n число
+ * @param k число-сравнение для делителей
+ * @return количество найденных делителей
+ */
+int countDivK(int n, const int k);
+
+/**
+ * @brief Точка входа в программу
+ * @return 0, если программа выполнена корректно, иначе 1
+ */
+int main()
+{
+    printf("Enter n: ");
+    int n = getValue();
+    checkPositive(n);
+    printf("Sum of %d in the sequence = %.4lf\n", n, getSumN(n));
+    printf("Enter e: ");
+    double e = getDouble();
+    checkPositive(e);
+    printf("Sum of numbers of the sequence with precision %lf = %.4lf\n", e, getSumE(e));
+    printf("Enter x:");
+    int x = getValue();
+    checkPositive(x);
+    printf("Enter k:");
+    int k = getValue();
+    checkPositive(k);
+    printf("The number of divisors of the number %d that are greater than %d = %d", x, k, countDivK(x,k));
+    return 0;
+}
+
+int getValue()
+{
+    int value = 0;
+    if (!scanf("%d", &value))
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    return value;
+}
+
+double getDouble()
+{
+    double value = 0;
+    if (!scanf("%lf", &value))
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    return value;
+}
+
+double getSumN(const int n)
+{
+    double current = -1;
+    double result = current;
+    for (int i = 0; i < n; i++ )
+    {
+        current *= getRecurent(i);
+        result += current;
+    }
+    return result;
+}
+
+double getRecurent(const int i)
+{
+    return (pow(-1, i)*(1+i))/(factorial(i));
+}
+
+void checkPositive(const double value)
+{
+    if (!value > 0)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+}
+
+double getSumE(const double e)
+{
+    double current = -1;
+    double result = 0;
+    for (int i = 1; fabs(current) > e; i++)
+    {
+        result += current;
+        current*=getRecurent(i);
+    }
+    return result;
+}
+
+int factorial(const int n)
+{
+    int result = 1;
+    for (int i = 1; i<=n; i++)
+    {
+        result*=i;
+    }
+    return result;
+}
+
+int countDivK(int n, const int k)
+{
+    int count = 0;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            if (i > k)
+            {
+                count++;
+            }
+            if (i != n / i && n / i > k)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
