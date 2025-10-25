7118f17a7e256c9aa44fcf2a7c1c86eeb3cceed9
Колесников Фёдор ТКИ-141 <kolesnikow_fedor@mail.ru>
2025-10-25 20:17:28 +0300

    Add files via upload

---
+++ task3.3.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <stdbool.h>
+#include <math.h>
+
+/**
+ * @brief считывает значение, введенное с клавиатуры с проверкой ввода
+ * @return считанное значение
+ */
+double getValue();
+
+/**
+ * @brief проверяет,что переменная положительная
+ * @param step значение проверяемой переменной
+ */
+void checkStep(const double step);
+
+/**
+ * @brief рассчитывает значение функции y по заданной формуле
+ * @param x значение 
+ * @return значение y
+ */
+double getY(const double x);
+
+/**
+ * @brief точка входа в программу
+ * @return 0, если программа выполнена корректно
+ */
+int main()
+{
+	printf("Enter starting value: ");
+	double start = getValue();
+	printf("Enter final value: ");
+	double end = getValue();
+	printf("Enter step: ");
+	double step = getValue();
+	checkStep(step);
+	for (double x = start; x < end + DBL_EPSILON; x = x + step)
+	{
+		printf("x = %.2lf, y = %.4lf\n", x, getY(x));
+	}
+	return 0;
+}
+
+double getValue()
+{
+	double value = 0;
+	if (!scanf_s("%lf", &value))
+	{
+		printf("Error\n");
+		abort();
+	}
+	return value;
+}
+
+void checkStep(const double step)
+{
+	if (step <= DBL_EPSILON)
+	{
+		printf("Error, step must be positive\n");
+		abort();
+	}
+}
+
+double getY(const double x)
+{
+	return pow(3,x);
+}
