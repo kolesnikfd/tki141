f1803f10b78fb583b05d70d70b32ea605be4de78
Колесников Фёдор ТКИ-141 <kolesnikow_fedor@mail.ru>
2025-10-27 18:44:03 +0300

    task1.3.c

--- task1.3.c
+++ task1.3.c
@@ -9,16 +9,23 @@
 double getHeat(const double m, const double t0, const double t);
 
 /**
+ * @brief сичтывает значение, введенное с клавиатуры с проверкой ввода
+ * @return считанное значение
+ */
+double getValue();
+
+/**
  * @brief точка входа в программу
  * @return возвращает 0, если программа выполнена корректно
  */
 int main(void)
 {
-    double m;
-    double t0;
-    double t;
-    printf("Print m(kg), t0 and t separated by space\n");
-    scanf("%lf %lf %lf", &m, &t0, &t);
+    printf("Enter m(kg):\n");
+    double m = getValue();
+    printf("Enter t0(sec):\n");
+    double t0 = getValue();
+    printf("Enter t(sec):\n");
+    double t = getValue();
     printf("Q = %.10f J", getHeat(m, t0, t));
     return 0;
 }
@@ -26,4 +33,15 @@ int main(void)
 double getHeat(const double m, const double t0, const double t)
 {
     return (500*m*(t-t0));
+}
+
+double getValue()
+{
+    double value=0;
+    if (!scanf("%lf", &value))
+    {
+        printf("Error\n");
+        abort();
+    }
+    return value;
 }
