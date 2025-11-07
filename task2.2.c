c0b6035c21eb1102064b24b91946a947f5e33eba
Колесников Фёдор ТКИ-141 <kolesnikow_fedor@mail.ru>
2025-11-07 20:36:40 +0300

    Update task2.2.c

--- task2.2.c
+++ task2.2.c
@@ -14,39 +14,40 @@ double getY (const double x, const double a);
  * @brief сичтывает значение, введенное с клавиатуры с проверкой ввода
  * @return считанное значение
  */
-double getValue();
+double getValue(void);
 
 /**
  * @brief точка входа в программу
  * @return 0, если программа выполнена корректно 
  */
-int main(void)
+int main()
 {
-    const double a=1.36;
-    double x=getValue();
-    printf("y = %.7f", getY(x,a));
+    const double a = 1.36;
+    double x = getValue();
+    printf("y = %.7g", getY(x,a));
     return 0;
 }
 
 double getY (const double x, const double a)
 {
-    if  (x<0.5)
+    if  (0.5 - x > __DBL_EPSILON__)
     {
-        return (M_PI_2 + x*a - exp(a*x));
+        return (M_PI_2 + x * a - exp(a*x));
     }
     else
     {
-        return (pow(x, 3)*a + 28);
+        return (pow(x, 3) * a + 28);
     }
 }
 
-double getValue()
+double getValue(void)
 {
     double value = 0;
-    if (!scanf("%lf",&value))
+    if (!scanf("%lg", &value))
     {
         printf("Error\n");
-        abort();
+        exit(0);
     }
     return value;
 }
+
