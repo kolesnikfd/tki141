b960c29448302fd8bdf6d97829a3b4463be91af7
Колесников Фёдор ТКИ-141 <kolesnikow_fedor@mail.ru>
2025-12-11 21:21:32 +0300

    Update task3.2.c

--- task3.2.c
+++ task3.2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 /**
@@ -28,6 +29,13 @@ double getSumN(const int n);
  */
 double getSumE(const double e);
 
+/**
+ * @brief рассчитывает коэффициент рекуррентного выражения
+ * @param i текущий индекс
+ * @return рассчитанное значение коэффициента
+ */
+double getRecurent(const int i);
+
 /** 
  * @brief проверяет, что число положительное
  * @param value - проверяемое значение
@@ -35,36 +43,20 @@ double getSumE(const double e);
 void checkPositive(const double value);
 
 /**
- * @brief ищет количество делителей числа n, которые больше k
- * @param n число
- * @param k число-сравнение для делителей
- * @return количество найденных делителей
- */
-int countDivK(int n, const int k);
-
-/**
  * @brief Точка входа в программу
  * @return 0, если программа выполнена корректно, иначе 1
  */
 int main()
 {
-    printf("Enter n: ");
+    printf("Enter n:");
     int n = getValue();
     checkPositive(n);
-    printf("Sum of %d in the sequence = %.20lf\n", n, getSumN(n));
-
-    printf("Enter e: ");
+    printf("Sum of %d in the sequence = %.20lf\n", n,getSumN(n));
+    printf("Enter e:");
     double e = getDouble();
     checkPositive(e);
-    printf("Sum of numbers of the sequence with precision %lf = %.20lf\n", e, getSumE(e));
+    printf("Sum of numbers of the sequence with precision %.10lf = %.20lf\n",e,getSumE(e));
 
-    printf("Enter x:");
-    int x = getValue();
-    checkPositive(x);
-    printf("Enter k:");
-    int k = getValue();
-    checkPositive(k);
-    printf("The number of divisors of the number %d that are greater than %d = %d", x, k, countDivK(x,k));
     return 0;
 }
 
@@ -90,62 +82,44 @@ double getDouble()
     return value;
 }
 
+double getSumN(const int n)
+{
+    double current = 1;
+    double result = current;
+    for (int i = 1; i < n; i++ )
+    {
+        current *= getRecurent(i);
+        result += current;
+    }
+    return result;
+}
+
+double getRecurent(const int i)
+{
+    return -1.0*(1+i)/pow(i,2);
+}
+
 void checkPositive(const double value)
 {
-    if (!value > __DBL_EPSILON__)
+    if (!value > 0)
     {
         printf("Error\n");
         exit(1);
     }
 }
 
-int countDivK(int n, const int k)
-{
-    int count = 0;
-    for (int i = 1; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            if (i > k)
-            {
-                count++;
-            }
-            if (i != n / i && n / i > k)
-            {
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
 double getSumE(const double e)
 {
-    double current = 1;
-    double result = 0.0;
-    double fact_part = 1;
-    double k = 0;
-    while ( fabs(current) > e )
+    double current = -1;
+    double result = 0;
+    for (int i = 1; fabs(current) > e; i++)
     {
-        result = result + current;
-        k++;
-        fact_part = fact_part * k;
-        current = (pow(-1, k) * (1 + k)) / fact_part;
+        result += current;
+        current*=getRecurent(i);
     }
+    return result;
+}
     return result;
 }
 
-double getSumN(const int n)
-{
-    double current = 1;
-    double result = 1.0;
-    double fact_part = 1;
-    for (int k = 1; k < n; k++)
-    {
-        fact_part = fact_part * k;
-        current = (pow(-1, k) * (1 + k)) / fact_part;
-        result = result + current;
-    }
-    return result;
-}
 
